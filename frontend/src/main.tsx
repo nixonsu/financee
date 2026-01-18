fca13f9e208cf580da291a1e84c7e8d4dbbd1b6c
nixonsu <nixon.s.uch@gmail.com>
2026-01-18 17:31:32 +1100

    feat: add frontend skeleton

---
+++ frontend/src/main.tsx
@@ -0,0 +1,10 @@
+import App from './App.tsx'
+import './globals.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
